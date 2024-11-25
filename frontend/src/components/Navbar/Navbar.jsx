761ffbd85884480179a67b3a58172b7d8e0195b9
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-11-25 22:47:33 +0530

    new

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" />
+      <img src={assets.logo} alt="" className="logo"/>
+      <ul className='navbar-menu'>
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
       
     </div>
   )
