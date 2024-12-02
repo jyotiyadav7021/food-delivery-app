bc7a4f56e9b803422581114cdbc0780b7bf39f7c
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-12-02 22:44:59 +0530

    new

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './Home.css'
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
 
@@ -10,8 +11,8 @@ const Home = () => {
         <div>
             <Header/>
             <ExploreMenu category={category} setCategory={setCategory}/>
-
-        </div>
+            <FoodDisplay category={category} />
+            </div>
     )
 }
 
