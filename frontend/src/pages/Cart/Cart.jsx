9535b846d1a93cbd4dbd8f64eadb61e84d99b4b9
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-12-10 14:02:07 +0530

    new

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import {StoreContext} from '../../context/StoreContext'
 function Cart() {
+
+  const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+
+
+
   return (
-    <div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p> Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p> Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          
+        })}
+      </div>
       
     </div>
   )
